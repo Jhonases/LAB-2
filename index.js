e21e5a8cce7c0f095b277197f462e13ae5271a7f
juanpetico <113464294+juanpetico@users.noreply.github.com>
2022-10-16 00:25:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function main() {
                 var data = JSON.parse(this.responseText);
                 chile(data);
             }
-            
+
             if (value == '2') {
                 url = 'espana.json';
                 var data = JSON.parse(this.responseText);
@@ -56,7 +56,21 @@ function main() {
     }
 }
 
-function chile(parsing){
+function agregarNombre() {
+    if (document.getElementById("mc1").value) {
+        var nombre = document.getElementById("mc1").value;
+        document.getElementById('nombre-mc1').innerHTML = nombre
+        document.getElementById('nombre-mc1-2').innerHTML = nombre
+    }
+
+    if (document.getElementById("mc2").value) {
+        var nombre = document.getElementById("mc2").value;
+        document.getElementById('nombre-mc2').innerHTML = nombre
+        document.getElementById('nombre-mc2-2').innerHTML = nombre
+    }
+}
+
+function chile(parsing) {
     var muestra = '<h1> Representantes de Chile</h1>' + '<ol>';
 
     for (let i in parsing[0].nombres) {
@@ -66,7 +80,7 @@ function chile(parsing){
     document.getElementById('mostrar').innerHTML = muestra;
 }
 
-function espana(parsing){
+function espana(parsing) {
     var muestra = '<h1> Representantes de España</h1>' + '<ol>';
 
     for (let i in parsing[0].nombres) {
@@ -76,7 +90,7 @@ function espana(parsing){
     document.getElementById('mostrar').innerHTML = muestra;
 }
 
-function mexico(parsing){
+function mexico(parsing) {
     var muestra = '<h1> Representantes de México</h1>' + '<ol>';
 
     for (let i in parsing[0].nombres) {
@@ -86,7 +100,7 @@ function mexico(parsing){
     document.getElementById('mostrar').innerHTML = muestra;
 }
 
-function argentina(parsing){
+function argentina(parsing) {
     var muestra = '<h1> Representantes de Argentina</h1>' + '<ol>';
 
     for (let i in parsing[0].nombres) {
@@ -95,3 +109,55 @@ function argentina(parsing){
     muestra += '</ol>';
     document.getElementById('mostrar').innerHTML = muestra;
 }
+
+function calcularResultado() {
+    // ! Fila 1  
+    let fila = document.getElementById("fila1");
+    let elementos = fila.getElementsByTagName("input");
+    let resultado = 0;
+    for (var i = 0; i <= 8; i++) {
+        resultado += parseInt(elementos[i].value);
+        console.log(elementos[i].value);
+    }
+    console.log(resultado);
+    document.getElementById('result1').innerHTML = resultado
+
+    // ! Fila 2  
+    let fila2 = document.getElementById("fila2");
+    let elementos2 = fila2.getElementsByTagName("input");
+    let resultado2 = 0;
+    for (var i = 0; i <= 8; i++) {
+        resultado2 += parseInt(elementos2[i].value);
+        console.log(elementos2[i].value);
+    }
+    console.log(resultado2)
+    document.getElementById('result2').innerHTML = resultado2;
+}
+
+function calcular_Resultado() {
+    // ! Fila 3  
+    let fila3 = document.getElementById("fila3");
+    let elementos3 = fila3.getElementsByTagName("input");
+    let resultado3 = 0;
+    for (var i = 0; i <= 8; i++) {
+        resultado3 += parseInt(elementos3[i].value);
+        console.log(elementos3[i].value);
+    }
+    console.log(resultado3)
+    document.getElementById('result3').innerHTML = resultado3;
+
+    // ! Fila 4  
+    let fila4 = document.getElementById("fila4");
+    let elementos4 = fila4.getElementsByTagName("input");
+    let resultado4 = 0;
+    for (var i = 0; i <= 8; i++) {
+        resultado4 += parseInt(elementos4[i].value);
+        console.log(elementos4[i].value);
+    }
+    console.log(resultado4)
+    document.getElementById('result4').innerHTML = resultado4;
+}
+
+
+
+
